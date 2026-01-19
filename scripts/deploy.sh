fd2d5b716df609ec89f0d6af66a57d06d338677f
Torben <59419684+entorb@users.noreply.github.com>
2026-01-19 06:04:50 +0100

    root

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,20 @@
 script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
 
+# exit upon error
+set -e
+
 rsync -rhv --delete --no-perms --ignore-times www/* entorb@entorb.net:html/flashcards/
 
-pnpm run check || exit 1
-pnpm run cy:run || exit 1
+pnpm run check
+pnpm run cy:run
 
-for app in 1x1 voc; do
-  pnpm run build:$app || exit 1
-  rsync -rhv --delete --no-perms --ignore-times apps/$app/dist/ entorb@entorb.net:html/$app/
-  rsync -rhv --delete --no-perms --ignore-times apps/$app/assets/icon.svg entorb@entorb.net:html/$app/icon.svg
+# migration
+rsync -rhv --delete --no-perms --ignore-times www/1x1/index.html entorb@entorb.net:html/1x1/
+rsync -rhv --delete --no-perms --ignore-times apps/voc/index.html entorb@entorb.net:html/voc/
+
+for app in 1x1 voc lwk; do
+  pnpm run build:$app
+  rsync -rhv --delete --no-perms --ignore-times apps/$app/dist/ entorb@entorb.net:html/fc-$app/
+  rsync -rhv --delete --no-perms --ignore-times apps/$app/assets/icon.svg entorb@entorb.net:html/fc-$app/icon.svg
 done
---
+++ todo-lwk.md
@@ -0,0 +1,6 @@
+# LWK App TODO
+
+## voc sync
+
+- align the app closer to voc, prevent code duplication if possible
+- move code to shared package if needed
