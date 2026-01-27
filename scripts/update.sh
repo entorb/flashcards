8590217f6876d02cc67ba3b9925c83e43f6c8315
Torben <59419684+entorb@users.noreply.github.com>
2026-01-27 07:15:26 +0100

    Tools

---
+++ scripts/update.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+echo === deleting old node_modules and lock===
+rm -rf node_modules
+rm -f pnpm-lock.yaml
+rm -rf packages/shared/node_modules
+rm -f packages/shared/pnpm-lock.yaml
+for app in 1x1 voc lwk; do
+  rm -rf $app/node_modules
+  rm -f $app/pnpm-lock.yaml
+done
+
+echo === updating root packages ===
+pnpm up
+
+echo === updating shared packages ===
+cd packages/shared
+pnpm up
+cd ../..
+
+for app in 1x1 voc lwk; do
+  echo === updating $app packages ===
+  cd apps/$app
+  pnpm up
+  cd ../..
+done
+
+
+echo === check ===
+pnpm run check
+
+echo === cypress ===
+echo "start 'pnpm run dev' and press Enter"
+read ok
+pnpm run cy:run
+
+echo DONE
