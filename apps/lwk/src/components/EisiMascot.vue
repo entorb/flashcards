bd717f85581a44beece17cf02d0a3453e56ffa67
Torben <59419684+entorb@users.noreply.github.com>
2026-01-27 07:16:09 +0100

    EisiMascot: remove body

--- apps/lwk/src/components/EisiMascot.vue
+++ apps/lwk/src/components/EisiMascot.vue
@@ -15,7 +15,7 @@ withDefaults(defineProps<Props>(), {
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 100 100"
+    viewBox="20 20 60 55"
     :width="size"
     :height="size"
     class="inline-block"
@@ -23,64 +23,56 @@ withDefaults(defineProps<Props>(), {
   >
     <title id="eisi-title">Eisi the Ice Bear</title>
     <g>
-      <!-- Body (ice blue/white) -->
-      <ellipse
-        cx="50"
-        cy="60"
-        rx="25"
-        ry="30"
-        fill="#E0F2F7"
-      />
       <!-- Head -->
       <circle
         cx="50"
-        cy="35"
+        cy="45"
         r="22"
         fill="#F0F8FF"
       />
       <!-- Ears -->
       <circle
         cx="35"
-        cy="20"
+        cy="30"
         r="8"
         fill="#E0F2F7"
       />
       <circle
         cx="65"
-        cy="20"
+        cy="30"
         r="8"
         fill="#E0F2F7"
       />
       <!-- Inner ears -->
       <circle
         cx="35"
-        cy="22"
+        cy="32"
         r="4"
         fill="#B0E0E6"
       />
       <circle
         cx="65"
-        cy="22"
+        cy="32"
         r="4"
         fill="#B0E0E6"
       />
       <!-- Eyes -->
       <circle
         cx="42"
-        cy="32"
+        cy="42"
         r="3"
         fill="#000000"
       />
       <circle
         cx="58"
-        cy="32"
+        cy="42"
         r="3"
         fill="#000000"
       />
       <!-- Nose -->
       <ellipse
         cx="50"
-        cy="40"
+        cy="50"
         rx="4"
         ry="3"
         fill="#000000"
@@ -88,7 +80,7 @@ withDefaults(defineProps<Props>(), {
       <!-- Grin (100% success) -->
       <path
         v-if="grin"
-        d="M 38,46 Q 50,58 62,46"
+        d="M 38,56 Q 50,66 62,56"
         stroke="#00BCD4"
         stroke-width="3"
         fill="none"
@@ -97,7 +89,7 @@ withDefaults(defineProps<Props>(), {
       <!-- Smile (70%+ success) -->
       <path
         v-else-if="smile"
-        d="M 40,46 Q 50,54 60,46"
+        d="M 40,56 Q 50,62 60,56"
         stroke="#4FC3F7"
         stroke-width="2.5"
         fill="none"
@@ -107,27 +99,12 @@ withDefaults(defineProps<Props>(), {
       <line
         v-else
         x1="42"
-        y1="46"
+        y1="56"
         x2="58"
-        y2="46"
+        y2="56"
         stroke="#000000"
         stroke-width="2"
         stroke-linecap="round"
-      />
-      <!-- Paws (front) -->
-      <ellipse
-        cx="35"
-        cy="75"
-        rx="8"
-        ry="10"
-        fill="#E0F2F7"
-      />
-      <ellipse
-        cx="65"
-        cy="75"
-        rx="8"
-        ry="10"
-        fill="#E0F2F7"
       />
     </g>
   </svg>
