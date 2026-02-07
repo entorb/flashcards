755b65c7121084c185220e30c044671feb03a7aa
Torben <59419684+entorb@users.noreply.github.com>
2026-02-07 22:51:53 +0100

    Update cypress.config.base.ts

--- cypress.config.base.ts
+++ cypress.config.base.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   screenshotOnRunFailure: false,
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    allowCypressEnv: false
   }
 })
