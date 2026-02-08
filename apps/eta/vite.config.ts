76d16eb7c406db5e06fd38398ec3e77633e71dcc
Torben <59419684+entorb@users.noreply.github.com>
2026-02-08 14:17:23 +0100

    1

--- apps/eta/vite.config.ts
+++ apps/eta/vite.config.ts
@@ -5,7 +5,7 @@ import { createAppViteConfig } from '../../vite.config.factory'
 export default createAppViteConfig({
   basePath: 'fc-eta',
   appTitle: 'Hausaufgaben Zeit-Schätzer',
-  description: 'Schätze die Restzeit für deine Hausaufgaben mit dem Eichhörnchen',
+  description: 'Schätze die Restzeit für deine Hausaufgaben',
   themeColor: '#8B4513',
   serverPort: 5176,
   previewPort: 4176,
