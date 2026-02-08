9291e1feea27f9fb5059bc70d9b48d89ea301df7
Torben <59419684+entorb@users.noreply.github.com>
2026-02-08 22:28:19 +0100

    Minor: DecksEditPage

--- apps/lwk/src/pages/DecksEditPage.vue
+++ apps/lwk/src/pages/DecksEditPage.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { DecksEditPage } from '@flashcards/shared/pages'
+import { useRouter } from 'vue-router'
 
 import { useGameStore } from '../composables/useGameStore'
 
+const router = useRouter()
 const { getDecks, addDeck, removeDeck, renameDeck } = useGameStore()
 
 function getNamingPattern() {
   return { prefix: 'Lernwörter_', startIndex: 1 }
+}
+
+function handleBack() {
+  router.push('/cards')
 }
 </script>
 
@@ -18,5 +24,6 @@ function getNamingPattern() {
     :remove-deck="removeDeck"
     :rename-deck="renameDeck"
     :get-naming-pattern="getNamingPattern"
+    @back="handleBack"
   />
 </template>
