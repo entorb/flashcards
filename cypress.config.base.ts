45ead063ec49dc303757bd1dc96ce2966aeeba64
Torben <59419684+entorb@users.noreply.github.com>
2026-02-25 01:33:23 +0100

    eslint and cypress flags

--- cypress.config.base.ts
+++ cypress.config.base.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   screenshotOnRunFailure: false,
+  video: false,
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     allowCypressEnv: false
