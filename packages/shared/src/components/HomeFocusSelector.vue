a89e76f714abeea57b1bcd25e54a9f5b3f0cb672
Torben <59419684+entorb@users.noreply.github.com>
2026-08-22 08:07:43 +0200

    focusOptions medium and strong removed

--- packages/shared/src/components/HomeFocusSelector.vue
+++ packages/shared/src/components/HomeFocusSelector.vue
@@ -11,8 +11,8 @@ const focus = defineModel<FocusType>({ required: true })
 
 const focusOptions = [
   { label: TEXT_DE.shared.focusOptions.weak, value: 'weak' as const, icon: 'trending_down' },
-  { label: TEXT_DE.shared.focusOptions.medium, value: 'medium' as const, icon: 'change_history' },
-  { label: TEXT_DE.shared.focusOptions.strong, value: 'strong' as const, icon: 'trending_up' },
+  // { label: TEXT_DE.shared.focusOptions.medium, value: 'medium' as const, icon: 'change_history' },
+  // { label: TEXT_DE.shared.focusOptions.strong, value: 'strong' as const, icon: 'trending_up' },
   { label: TEXT_DE.shared.focusOptions.slow, value: 'slow' as const, icon: 'schedule' }
 ]
 </script>
@@ -29,7 +29,7 @@ const focusOptions = [
       <div
         v-for="option in focusOptions"
         :key="option.value"
-        class="col-6 col-sm-3"
+        class="col-6 col-sm-6"
       >
         <q-btn
           :outline="focus !== option.value"
