d746a872adbd860d61f33ca71ddc9932c3264706
Torben <59419684+entorb@users.noreply.github.com>
2026-08-26 05:04:05 +0200

    fix: cypress ci flanky

--- cypress.config.base.ts
+++ cypress.config.base.ts
@@ -5,6 +5,11 @@ export default defineConfig({
   video: false,
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    allowCypressEnv: false
+    allowCypressEnv: false,
+    defaultCommandTimeout: 8000,
+    retries: {
+      runMode: process.env.CI ? 2 : 0,
+      openMode: 0
+    }
   }
 })
